62590099c908b549bad0ee1e5da9461af54104bf
bui_khoi <buikhoi100498@gmail.com>
2019-09-24 16:13:48 +0700

    init project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+  get  "static_pages/home"
+  get  "static_pages/help"
+  get  "static_pages/contact"
+  get  "static_pages/about"
+  end
+end
